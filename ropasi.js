5046aba741245ac66fb7fc52823637284bb56216
jdogknigh <jdegraffe313@gmail.com>
2022-07-21 21:02:44 -0400

    ropasi.js

--- ropasi.js
+++ ropasi.js
@@ -3,3 +3,12 @@
 // compairs pick for winning//
 // if winning say you win if lose say you lose//
 //now for a restart//
+
+
+// computer must generate one of the three random options
+//player has options to pick one of the three
+    //player input
+//compair the two choices using r > s, s > p, p >r
+// if its p , c its victory if its c , p then lost and if
+// r = r , s = s, p=p its tie. 
+//output outcome./ 
