12bf543e3acb2a8870367852da0cd913162907f8
jdogknigh <jdegraffe313@gmail.com>
2022-07-22 23:58:01 -0400

    ropasi.html

--- ropasi.js
+++ ropasi.js
@@ -12,3 +12,43 @@
 // if its p , c its victory if its c , p then lost and if
 // r = r , s = s, p=p its tie. 
 //output outcome./ 
+function getCompterchoice(){
+    let randomNumber = Math.floor((Math.random()) * 3);
+console.log (randomNumber)
+  if (randomNumber=== (0) )
+    {console.log ("rock")}
+else if (randomNumber === (1) )
+    console.log ("paper")
+else 
+    console.log ("scissors")
+
+}
+//code above works for ouput of will need to place all input into lowercase
+//code to create computer selection 
+
+function playerSelection(){
+    const guesses = document.querySelector('.guesses');
+    const guessField = document.querySelector('.guessSubmit');
+let playerSelection = guessField.string
+console.log (playerSelection)
+function play (){
+    if (playerSelection === getCompterchoice)
+    return 'Draw!'
+    else 
+    return 'fail'           //cannot get player input and computer to output anything
+}
+}
+console.log (playerSelection)
+//code for player input
+//function playRound(playerSelection, computerSelection){
+//code
+
+//const playerSelection = "rock";
+//const computerSelection = getCompterchoice();
+//console.log(playRound(playerSelection, computerSelection))
+      
+    
+//function game ()
+
+//for (let i=0; i < 5; i++){
+    //code
